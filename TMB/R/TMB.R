917aa401818e30f376f95c26c08250244a1e3e89
John Wallace <John.Wallace@noaa.gov>
2022-02-04 19:01:16 -0800

    compile(), flags default "-Wno-ignored-attributes"
    For straight CRAN R ver 4.2.1, the newly added <-I".../RcppEigen/include" > to the call throws warnings; this flag stops those warnings.

--- TMB/R/TMB.R
+++ TMB/R/TMB.R
@@ -1012,7 +1012,7 @@ openmp <- function(n=NULL){
 ##' @param tracesweep Turn on preprocessor flag to trace AD sweeps? (Silently disables \code{libtmb})
 ##' @param ... Passed as Makeconf variables.
 ##' @seealso \code{\link{precompile}}
-compile <- function(file,flags="",safebounds=TRUE,safeunload=TRUE,
+compile <- function(file,flags="-Wno-ignored-attributes",safebounds=TRUE,safeunload=TRUE,
                     openmp=isParallelTemplate(file[1]),libtmb=TRUE,
                     libinit=TRUE,tracesweep=FALSE,remove_arg_Wall = TRUE,...){  
   if(.Platform$OS.type=="windows"){
